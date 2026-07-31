dfca15cd89ebb532d176e2196ae56c78de1a319d
Karan <karan5kumarrr@gmail.com>
2026-07-31 11:30:06 +0530

    Time: 53 ms (69.49%) | Memory: 21.3 MB (54.84%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<pair<string, int>> q;
-        q.push({beginWord, 1});
         unordered_set<string> st(wordList.begin(), wordList.end());
         st.erase(beginWord);
+
+        queue<pair<string, int>>q;
+        q.push({beginWord, 1});
         while(!q.empty()){
             string word = q.front().first;
-            int steps = q.front().second;
+            int dis = q.front().second;
             q.pop();
             if (word == endWord){
-                return steps;
+                return dis;
             }
             for (int i=0; i<word.size(); i++){
-                char original = word[i];
+                char org = word[i];
                 for (char j='a'; j<='z'; j++){
                     word[i] = j;
-                    if (st.find(word) != st.end()){
+                    if(st.find(word) != st.end()){
                         st.erase(word);
-                        q.push({word, steps +1});
+                        q.push({word, dis+1});
                     }
                 }
-                word[i] = original;
+                word[i] = org;
             }
+
         }
         return 0;
-
-
     }
 };
